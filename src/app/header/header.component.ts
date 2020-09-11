fa23c32ed61263905a4df6015d2f53b6cfd84dfe
Yichuan Ma <ma.yich@husky.neu.edu>
2020-09-11 05:04:05 -0400

    header just about done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
